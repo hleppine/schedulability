d4d5f1a3c13d169b06c88c65852fd2fcbe4c0f3a
hleppine <hannu.leppinen@gmail.com>
2023-05-06 19:01:11 +0300

    Update sim.py

--- src/sim.py
+++ src/sim.py
@@ -1,1 +1,7 @@
+""" A simple schedulability analysis simulator.
+
+The simulator can be used to analyse the schedulability
+of task sets with fixed-priority pre-emptive scheduling.
+"""
+
 print("Hello World!")

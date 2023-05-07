84a7ba3256aed4c0c254c934f6ba17509d498c52
Hannu Leppinen <hannu.leppinen@gmail.com>
2023-05-07 19:07:54 +0300

    pylint fixes

--- tests/tests.py
+++ tests/tests.py
@@ -1,7 +1,17 @@
+""" Tests for the schedulability simulator.
+
+TODO: Further documentation.
+"""
+
 from sched.sim import Simulator, Task
 
 
 def test():
+    """
+    Simple tests.
+
+    Test some task sets with expected results from literature.
+    """
     task_set = [
         Task(
             name="a",
@@ -30,8 +40,8 @@ def test():
     ]
     sim = Simulator()
     sim.run(task_set, 60)
-    for x in sim.timeline:
-        print(f"{x[0]} .. {x[1]}: {x[2]}")
+    for item in sim.timeline:
+        print(f"{item[0]} .. {item[1]}: {item[2]}")
 
 
 if __name__ == "__main__":

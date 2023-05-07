c27f17895f39f5cf585b08b7b3d5d5142441a94b
Hannu Leppinen <hannu.leppinen@gmail.com>
2023-05-07 18:39:07 +0300

    Add mistakenly omitted files

---
+++ tests/tests.py
@@ -0,0 +1,39 @@
+from sched.sim import Simulator
+from sched.sim import Task
+
+
+def test():
+    task_set = [
+        Task(
+            name="a",
+            priority=1,
+            period=50,
+            deadline=50,
+            offset=0,
+            wcet=12,
+        ),
+        Task(
+            name="b",
+            priority=2,
+            period=40,
+            deadline=40,
+            offset=0,
+            wcet=10,
+        ),
+        Task(
+            name="c",
+            priority=3,
+            period=30,
+            deadline=30,
+            offset=0,
+            wcet=10,
+        ),
+    ]
+    sim = Simulator()
+    sim.run(task_set, 50)
+    for x in sim.timeline:
+        print(f"{x[0]} .. {x[1]}: {x[2]}")
+
+
+if __name__ == "__main__":
+    test()
